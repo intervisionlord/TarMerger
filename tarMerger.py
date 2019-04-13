05c84b547adba059377b5cf0ac2fc77183c464c4
intervisionlord <ads-net@mail.ru>
2019-04-13 17:48:51 +0300

    version 1.1.4
    + Добавлен вывод версии
    + Добавлено ожидание нажатия перед закрытием
    + Добавлены цвета

--- tarMerger.py
+++ tarMerger.py
@@ -1,11 +1,17 @@
-# tarMerger
-# v.: 1.0
-# Intervision ©
 
-import glob, os, sys
+softName = 'TarMerger'
+ver = '1.1.4'
+author = 'Intervision'
 
+about = [softName, author, ver]
 
-print('Укажите путь до директории с архивами или пропустите этот шаг, если операция выполняется в текущей директории')
+import glob, os, sys, crayons, colorama
+
+colorama.init()
+print(crayons.cyan('{} by {}\nVesion:{}', bold=True).format(*about))
+print('====================')
+
+print('Укажите путь до директории с архивами или пропустите этот шаг,\nесли операция выполняется в текущей директории')
 tarDir = input()
 if tarDir == "":
       tarDir = os.getcwd()
@@ -18,7 +24,7 @@ else:
 try:
     os.chdir(tarDir)
 except FileNotFoundError:
-    print('ERR: #010: Указанная директория отсутствует или задана неверно.')
+    print(crayons.red('ERR: #010:', bold=True) + ' Указанная директория отсутствует или задана неверно.')
     sys.exit("Остановка выполнения")
 print('====================')
 if glob.glob("*.tgz"):
@@ -26,7 +32,9 @@ if glob.glob("*.tgz"):
     for file in glob.glob("*.tgz"):
         print(file)
 else:
-    print('ERR: #310: Архивы отсутствуют')
+    print(crayons.yellow('ERR: #310:', bold=True) + ' Архивы отсутствуют')
+    print('Нажмите любую клавишу для завершения')
+    exitKey = input()
     sys.exit("Остановка выполнения")
 
 print('====================')
@@ -55,8 +63,9 @@ elif yesNo == "n":
       sys.exit('Остановка выполнения')
 
 elif yesNo == "y":
-      print('Для подтверждения используйте букву Y (регистр имеет значение!)')
+      print(crayons.yellow('ERR: #302:', bold=True) + ' Для подтверждения используйте букву Y (регистр имеет значение!)')
       sys.exit('Регистр ответа неверен')
 else:
-      print('ERR: #302: Команда не распознана')
+      print(crayons.yellow('ERR: #303:', bold=True) + ' Команда не распознана')
+      print('Нажмите любую клавишу для завершения')
       sys.exit('Команда не распознана')
